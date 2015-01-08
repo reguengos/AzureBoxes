769a314648ba421ae281b8e7e00871b0190e5917
Branko Straub <b.straub@outlook.com>
2015-01-08 14:31:23 -0300

    rollback

--- script.js
+++ script.js
@@ -44,14 +44,5 @@ function filter() {
 		else {
 			$(this).addClass('not-third');
 		}
-		
-		if(((index)%2)==0){
-			$(this).addClass('derecha');
-		}
-		else{
-			$(this).addClass('izquierda');
-		}
-
-		}
 	});
 }
